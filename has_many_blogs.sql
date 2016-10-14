3763bd2cbea27191e5df0288969203151b216c98
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2016-10-14 10:24:59 -1000

    created tables

---
+++ has_many_blogs.sql
@@ -0,0 +1,51 @@
+ \c janphillipcarreon
+
+ DROP USER IF EXISTS has_many_user;
+
+ CREATE USER has_many_user;
+
+ DROP DATABASE IF EXISTS has_many_blogs;
+
+ CREATE DATABASE has_many_blogs OWNER has_many_user;
+
+ \c has_many_blogs;
+
+ \i scripts/blog_data.sql;
+
+ DROP TABLE IF EXISTS users;
+
+ CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username character varying(90),
+  first_name character varying(90) DEFAULT NULL,
+  last_name character varying(90) DEFAULT NULL,
+  created_at timestamp with time zone DEFAULT now(),
+  updated_at timestamp with time zone DEFAULT now()
+  );
+
+ DROP TABLE IF EXISTS posts;
+
+ CREATE TABLE posts (
+  id serial PRIMARY KEY,
+  title character varying(180) DEFAULT NULL,
+  url character varying(510) DEFAULT NULL,
+  content text DEFAULT NULL,
+  created_at timestamp with time zone DEFAULT now(),
+  updated_at timestamp with time zone DEFAULT now()
+  );
+
+ DROP TABLE IF EXISTS comments;
+
+ CREATE TABLE comments (
+  id serial PRIMARY KEY,
+  body character varying(510) DEFAULT NULL,
+  created_at timestamp with time zone DEFAULT now(),
+  updated_at timestamp with time zone DEFAULT now()
+  );
+
+
+
+
+
+
+
